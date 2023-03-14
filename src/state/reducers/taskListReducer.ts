9045aac8327965fc802cda91a79ffcb7655c7890
MartynaMichalska <martyna.michalska@void.pl>
2023-03-14 14:08:18 +0100

    fix: remove tasks not working

--- src/state/reducers/taskListReducer.ts
+++ src/state/reducers/taskListReducer.ts
@@ -2,17 +2,14 @@ import { Task } from "../../interfaces";
 import { ListAction } from "../actions";
 const initialState: Task[] = [];
 
-
-
 const taskListReducer = (state: Task[] = initialState, action: ListAction) => {
   switch (action.type) {
     case "addTask":
       return [...state, action.task];
     case "removeTask":
-      state.filter((task) => {
+      return state.filter((task) => {
         return task.id !== action.task.id;
       });
-      return state;
     default:
       return state;
   }
